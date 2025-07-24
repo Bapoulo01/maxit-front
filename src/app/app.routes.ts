ed0269f0babd70a4555bc7b51f37c6ba93393a36
Bapoulo01 <amdoux2.0@gmail.com>
2025-07-24 17:15:44 +0000

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Demande } from './pages/demande/demande';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        component: Login
+    },
+    {
+        path:'demande',
+        component: Demande
+    },
+    {
+        path:'dashboard',
+        component: Dashboard
+    },
+    {
+        path:'',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+];
---
+++ src/app/pages/demande/demande.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demande',
+  imports: [],
+  templateUrl: './demande.html',
+  styleUrl: './demande.css'
+})
+export class Demande {
+
+}
